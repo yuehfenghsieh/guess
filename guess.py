23f2fe39effe55d2ffcf4edf5dce0a1b61d8c601
yuehfenghsieh <jason70160@gmail.com>
2023-10-26 22:08:42 +0800

    version03

--- guess.py
+++ guess.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1, 100)
+start = input('輸入猜數字範圍下限')
+end = input('請輸入猜數字範圍上限')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1
